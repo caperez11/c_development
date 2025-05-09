eabae4e4006ced1eedb19a40a6c68adf0206c008
caperez11 <alejandroperez499@gmail.com>
2025-05-08 20:44:13 -0500

    add; ejercicio_9.c y ejercicio_10.c

--- practica_1/ejercicio_10.c
+++ practica_1/ejercicio_10.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-void main(){
+void main() {
+    // Declarar variables
+    int dia_semana = 0;
 
+    // Solicitar datos
+    printf("Ingrese el número del día de la semana (1-7): ");
+    scanf("%d", &dia_semana);
+
+    if (dia_semana == 1) {
+        printf("Lunes\n");
+    } else if (dia_semana == 2) {
+        printf("Martes\n");
+    } else if (dia_semana == 3) {
+        printf("Miércoles\n");
+    } else if (dia_semana == 4) {
+        printf("Jueves\n");
+    } else if (dia_semana == 5) {
+        printf("Viernes\n");
+    } else if (dia_semana == 6) {
+        printf("Sábado\n");
+    } else if (dia_semana == 7) {
+        printf("Domingo\n");
+    } else {
+        printf("Número de día inválido. Debe estar entre 1 y 7.\n");
+    }
 }
--- practica_1/ejercicio_9.c
+++ practica_1/ejercicio_9.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-void main(){
+void main() {
+    // Declarar variables
+    double peso = 0.0;
 
+    // Solicitar datos
+    printf("Ingrese el peso de la persona en kilogramos: ");
+    scanf("%lf", &peso);
+
+    if (peso < 18.5) {
+        printf("Bajo Peso\n");
+    } else if (peso <= 24.9) {
+        printf("Normal\n");
+    } else if (peso <= 29.9) {
+        printf("Sobrepeso\n");
+    } else {
+        printf("Obesidad\n");
+    }
 }
